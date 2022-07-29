1a87c362fc724472a4e90821a36b4c1aa9b1bd93
shin-chapi <sakashin62@gmail.com>
2022-07-29 13:01:00 +0900

    view edit

---
+++ src/main/java/com/example/controllers/SignupController.java
@@ -0,0 +1,23 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+;
+
+@Controller
+public class SignupController {
+	
+	
+	
+	
+	@GetMapping("/signup")
+    public String signup( ) {
+		
+    	return "signup";
+    }
+	
+	 
+	 
+}
+
---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+/**
+ * ユーザー情報 Repository
+ */
+//@Repository
+//public interface UserRepository extends JpaRepository<Users, Integer> {}
