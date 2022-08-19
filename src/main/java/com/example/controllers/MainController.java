3442e8025f7e21a7bde3aacb14e2fad859902f87
shin-chapi <sakashin62@gmail.com>
2022-08-19 17:00:48 +0900

    update

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,7 +1,12 @@
 package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.form.PostForm;;
 
 @Controller
 public class MainController {
@@ -12,7 +17,20 @@ public class MainController {
 	}
 
 	@PostMapping("/create")
-	public String create() {
-		return "post";
+	public String create(@ModelAttribute PostForm postform) {
+		return "postEdit";
 	}
+	
+	@PostMapping("edit")
+	public String edit(@ModelAttribute @Validated PostForm postform, BindingResult bindingResult) {
+		
+		// 入力チェック結果
+				if (bindingResult.hasErrors()) {
+					// NG:ユーザー登録画面に戻ります
+					return "postEdit";
+				}
+		
+		return "calendar";
+	}
+	
 }
--- src/main/java/com/example/form/PostForm.java
+++ src/main/java/com/example/form/PostForm.java
@@ -3,6 +3,12 @@ package com.example.form;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -10,22 +16,30 @@ public class PostForm {
 
 	private Long id;
 
+	
 	private int categoryId;
-
+	
+	@NotNull(message="日付を入力してください")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date diaryDay;
-
+	
+	@NotBlank
+	@Size(max=50,message="50文字以内で入力してください")
 	private String record1;
-
+	
+	@NotBlank
+	@Size(max=50,message="50文字以内で入力してください")
 	private String record2;
-
+	
+	@NotBlank
+	@Size(max=50,message="50文字以内で入力してください")
 	private String record3;
-
-	private String imageName;
-
+	
+	@NotBlank
+	@Size(max=100,message="100文字以内で入力してください")
 	private String memo;
-
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime createAt;
-
-	private LocalDateTime updateAt;
 
 }
