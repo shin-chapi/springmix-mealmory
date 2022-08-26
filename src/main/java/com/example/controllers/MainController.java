ca2f240178644c00b43f2ad37d88c7721caa9f5e
shin-chapi <sakashin62@gmail.com>
2022-08-26 13:28:22 +0900

    update

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,13 +1,19 @@
 package com.example.controllers;
 
+import java.time.LocalDateTime;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.form.FileUploadForm;
 import com.example.form.PostForm;
 import com.example.model.User;
+import com.example.service.FileUploadService;
 import com.example.service.PostRecordService;;
 
 @Controller
@@ -15,9 +21,12 @@ public class MainController {
 	
 	private final PostRecordService postRecordService;
 	
-	public MainController(PostRecordService postRecordService
+	private final FileUploadService fileUploadService;
+	
+	public MainController(PostRecordService postRecordService,FileUploadService fileUploadService
 			) {
 		this.postRecordService = postRecordService;
+		this.fileUploadService = fileUploadService;
 		
 	}
 
@@ -32,16 +41,25 @@ public class MainController {
 	}
 	
 	@PostMapping("/edit")
-	public String edit(User user, @ModelAttribute("postform") @Validated PostForm postform, BindingResult bindingResult ) {
+	public String edit( @AuthenticationPrincipal User details,
+			            @ModelAttribute("postform") @Validated PostForm postform, BindingResult bindingResult,
+			            @ModelAttribute("fileUploadForm")  @Validated FileUploadForm file,BindingResult resultFile,Model model) throws Exception{
 		
 		// 入力チェック結果
-				if (bindingResult.hasErrors()) {
+				if (bindingResult.hasErrors()|| resultFile.hasErrors()) {
 					System.out.println(bindingResult);
 					// NG:ユーザー登録画面に戻ります
 					return "postEdit";
 					
 				}
+				String imageName = null;
+				LocalDateTime dateTime = LocalDateTime.now();
+				file.setCreateAt(dateTime);
+				imageName = fileUploadService.fileUpload(file);
 				
+				postform.setUserName(details.getUsername());
+				postform.setCreateAt(dateTime);
+				postform.setImageName(imageName);
 				postRecordService.insertDiaryRecord(postform);
 			
 		
---
+++ src/main/java/com/example/service/FileUploadService.java
@@ -0,0 +1,18 @@
+package com.example.service;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+import com.example.form.FileUploadForm;
+
+
+@Service
+public class FileUploadService {
+
+	
+	public String fileUpload(FileUploadForm fileUploadForm) 
+		       throws IOException{
+				return null;
+}
+}
