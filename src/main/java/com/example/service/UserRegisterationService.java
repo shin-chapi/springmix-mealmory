3660b50e902eab6330ef41e1c13e3f4a7ca752a1
shin-chapi <sakashin62@gmail.com>
2022-08-31 12:10:48 +0900

    update

--- src/main/java/com/example/service/UserRegisterationService.java
+++ src/main/java/com/example/service/UserRegisterationService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,19 +16,23 @@ public class UserRegisterationService {
 	@Autowired
 	public UserMapper userMapper;
 	
-//	@Autowired
+	
 	public BCryptPasswordEncoder encoder;
 	
 	@Transactional
 	public  void registerUser(User user) {
 		
 		
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		encoder  = context.getBean(BCryptPasswordEncoder.class);
 		
 		
 		String rawPassword = user.getPassword();
 		user.setPassword(encoder.encode(rawPassword));
 		
 		userMapper.registerUser(user);
+		context.close();
 		
 	}
 	
